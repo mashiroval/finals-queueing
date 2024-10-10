9b1d06fb3129c2d161511d465b108140433f62d5
mashiroval <vincentnuilan@gmail.com>
2024-10-10 15:03:16 +0800

    TEST GELO

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TEST WORKING
+          TEST GELO
         </p>
         <a
           className="App-link"
