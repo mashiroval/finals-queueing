048589963434e4d8628d4eda11e2a55ceb95a7c1
mashiroval <vincentnuilan@gmail.com>
2024-10-15 16:16:36 +0800

    show NavBar

--- src/pages/AdminHomePage.js
+++ src/pages/AdminHomePage.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import logo from "../logo.png";
 import './pagestyles.css';
+import NavBar from '../components/NavBar';
 
 function AdminHomePage() {
   return (
     <div>
+        <NavBar/>
       <div className="vlogoadmin">
       <Link to={"/"}>
         <img src={logo} width={250} height={200} alt="Logo" />
--- src/pages/AdminQueuePage.js
+++ src/pages/AdminQueuePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from "../logo.png";
+import NavBar from '../components/NavBar';
 
 function AdminQueuePage() {
   return (
     <div>
+         <NavBar/>
       <div className="vlogoadmin">
       <Link to={"/"}>
         <img src={logo} width={250} height={200} alt="Logo" />
