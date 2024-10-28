a7aafb9bb03c54b0be36388c2602636433fe565d
AngeloGNZ <Gabrielnunez002@gmail.com>
2024-10-28 12:32:10 +0800

    new

--- src/pages/AdminHomePage.js
+++ src/pages/AdminHomePage.js
@@ -7,19 +7,56 @@ import './pagestyles.css';
 
 function AdminHomePage() {
   const [users, setUsers] = useState([]);
+  const [waiting, setWaiting] = useState([]);
+
+  const [counterTickets, setCounterTickets] = useState({
+    Register: null,
+    Withdraw: null,
+    Deposit: null,
+    Service: null,
+  });
 
   const fetchUsers = () => {
     axios.get("http://localhost:8000/api/getUsers")
       .then((response) => {
-        setUsers(response.data);
+        const fetchedUsers = response.data;
+  
+        console.log("Fetched Users:", fetchedUsers);
+  
+        const waitingList = [];
+        const counterAssignments = {
+          Register: null,
+          Withdraw: null,
+          Deposit: null,
+          Service: null,
+        };
+  
+        const unsettledUsers = fetchedUsers.filter(user => user.settled === false || user.settled === "false");
+        console.log("Unsettled Users (should be only those with settled: false):", unsettledUsers);
+  
+        unsettledUsers.forEach(user => {
+          if (!counterAssignments[user.type]) {
+            counterAssignments[user.type] = user;
+          } else {
+            waitingList.push(user);
+          }
+        });
+  
+        console.log("Counter Assignments:", counterAssignments);
+        console.log("Waiting List:", waitingList);
+  
+        setCounterTickets(counterAssignments);
+        setWaiting(waitingList);
       })
       .catch((error) => {
-        console.log(error);
+        console.log("Error fetching users:", error);
       });
   };
 
   useEffect(() => {
     fetchUsers();
+    const interval = setInterval(fetchUsers, 4000);
+    return () => clearInterval(interval);
   }, []);
 
   return (
@@ -30,25 +67,29 @@ function AdminHomePage() {
 <div className='cardcontainer'>
         <div className='box1'>
           <h3 className="admins3">OVERVIEW</h3>
-            {/* <iframe 
-              style={{
+          <iframe 
+            style={{
               background: '#FFFFFF',
               border: 'none',
               borderRadius: '2px',
               boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
-              width:'50vw',
-              height:'50vh'
-              }}
-            src="https://charts.mongodb.com/charts-project-0-kdrfole/embed/charts?id=8bd7ae8d-79c6-4386-b2a9-7fa824a5f3a3&maxDataAge=3600&theme=light&autoRefresh=true"/>
-            {users.map(user => (
-                <p key={user._id}>
-                    {user.ticketnum} - {user.showed} - {user.settled}
-                </p>
-              ))} */}
+              width:'650px',
+              height:'500px'
+            }}
+            src="https://charts.mongodb.com/charts-project-0-kdrfole/embed/charts?id=8dcb76b1-b0f2-4e21-9c53-1241d89293f7&maxDataAge=3600&theme=light&autoRefresh=true"
+            />
         </div>
 
         <div className='box2'>
-        <h3 className="admins4">SERVING</h3>
+        <h3 className="admins4">NOW SERVING</h3>
+            <div className='admin-container' id="column">
+              {["Register", "Withdraw", "Deposit", "Service"].map((type, index) => (
+                <div key={type}>
+                  <p>
+                    {counterTickets[type] ? counterTickets[type].ticketnum : "No ticket"}
+                  </p>
+              </div>))}
+            </div>
         </div>
 </div>
         
